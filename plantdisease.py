0c4cbd0790560f4ca4a116770a59e5971a8bb188
Shaolin Kataria <48819365+shaolink8@users.noreply.github.com>
2019-09-23 23:29:27 +0530

    Create plantdisease.py

---
+++ plantdisease.py
@@ -0,0 +1,58 @@
+# Convolutional Neural Network
+
+
+
+# Importing the Keras libraries and packages
+from keras.models import Sequential
+from keras.layers import Convolution2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
+
+
+classifier = Sequential()
+
+
+classifier.add(Convolution2D(32, 3, 3, input_shape = (64, 64, 3), activation = 'relu'))
+
+
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+classifier.add(Convolution2D(32, 3, 3, activation = 'relu'))
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+classifier.add(Flatten())
+classifier.add(Dense(output_dim = 128, activation = 'relu'))
+classifier.add(Dense(output_dim = 1, activation = 'sigmoid'))
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+# Part 2 - Fitting the CNN to the images
+
+from keras.preprocessing.image import ImageDataGenerator
+
+train_datagen = ImageDataGenerator(rescale = 1./255,
+                                   shear_range = 0.2,
+                                   zoom_range = 0.2,
+                                   horizontal_flip = True)
+
+test_datagen = ImageDataGenerator(rescale = 1./255)
+
+training_set = train_datagen.flow_from_directory('dataset/training_set',
+                                                 target_size = (64, 64),
+                                                 batch_size = 32,
+                                                 class_mode = 'binary')
+
+test_set = test_datagen.flow_from_directory('dataset/test_set',
+                                            target_size = (64, 64),
+                                            batch_size = 32,
+                                            class_mode = 'binary')
+
+classifier.fit_generator(training_set,
+                         samples_per_epoch = 8000,
+                         nb_epoch = 1,
+                         validation_data = test_set,
+                         nb_val_samples = 2000)
+predict_set = ImageDataGenerator().flow_from_directory('dataset/predict_set', 
+                                                      target_size = (64, 64), 
+                                                      batch_size = 32, 
+                                                      class_mode = 'categorical')
+ 
+prediction = classifier.predict_generator(predict_set, verbose = 1, steps = 1)
